aa0b45bde74522b33577ed315a1c1c0b569b5c6b
THEEKSHANA <87897206+ImTheekshana126@users.noreply.github.com>
2021-10-24 20:10:09 +0530

    Sophia

--- Sophia/modules/admin.py
+++ Sophia/modules/admin.py
@@ -712,7 +712,7 @@ dispatcher.add_handler(SETCHAT_TITLE_HANDLER)
 dispatcher.add_handler(SETSTICKET_HANDLER)
 dispatcher.add_handler(SETDESC_HANDLER)
 
-__mod_name__ = "💞Admin💞"
+__mod_name__ = "Admin"
 __command_list__ = [
     "adminlist",
     "admins",
