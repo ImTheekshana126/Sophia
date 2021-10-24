645780229abff9298ad4828403b178de320d537d
THEEKSHANA <87897206+ImTheekshana126@users.noreply.github.com>
2021-10-24 20:17:22 +0530

    Sophia

--- Sophia/modules/backups.py
+++ Sophia/modules/backups.py
@@ -368,7 +368,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "💞Backup💞"
+__mod_name__ = "Backup"
 
 __help__ = """
 *Only for group owner:*
