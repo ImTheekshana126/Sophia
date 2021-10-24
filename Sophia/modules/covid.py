13bc2c74e4eb5765fc604e93392c03b05b150924
THEEKSHANA <87897206+ImTheekshana126@users.noreply.github.com>
2021-10-24 20:24:39 +0530

    Sophia

--- Sophia/modules/covid.py
+++ Sophia/modules/covid.py
@@ -23,7 +23,7 @@ def covid(update: Update, context: CallbackContext):
 COVID_HANDLER = DisableAbleCommandHandler(["covid", "corona"], covid)
 dispatcher.add_handler(COVID_HANDLER)
 
-__mod_name__ = "💞Covid-19💞"
+__mod_name__ = "Covid-19"
 
 __help__ = """
 ~ `/covid`** <songname artist(optional)>: download the song in it's best quality available.(API BASED)
