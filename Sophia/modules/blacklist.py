05256d035dbd247b375d502b2d642243ef4ac961
THEEKSHANA <87897206+ImTheekshana126@users.noreply.github.com>
2021-10-24 20:19:01 +0530

    Sophia

--- Sophia/modules/blacklist.py
+++ Sophia/modules/blacklist.py
@@ -448,7 +448,7 @@ def __stats__():
     )
 
 
-__mod_name__ = "💞blacklist💞"
+__mod_name__ = "blacklist"
 
 __help__ = """
 Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
